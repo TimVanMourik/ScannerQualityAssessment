b0bcaf2858f632d31d7bcedfac63fa52dcf107e7
Tim van Mourik <timvanmourik@gmail.com>
2017-07-27 15:00:08 +0200

    Make sure the log files don't appear in the repo

--- docker/run-website.sh
+++ docker/run-website.sh
@@ -1,1 +1,6 @@
+# run docker
 docker run -3000:3000 -v `pwd`/db:/qaweb/db -v `pwd`/public:/qaweb/public qawebsite_web bundle exec rails s
+
+#push image to docker hub
+#docker tag qawebsite_web timvanmourik/scannerqualityassessment:1.0
+#docker push timvanmourik/scannerqualityassessment:1.0
